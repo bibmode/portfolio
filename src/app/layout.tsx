f91617b67d4f456c5c871de2e410f685d8a52355
Genevieve <vievenavales29@gmail.com>
2023-10-29 07:40:50 +0800

    edited metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 import "./globals.css";
-import type { Metadata } from "next";
+import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import React, { useEffect, useState } from "react";
